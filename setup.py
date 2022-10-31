2433811c6fdbd3920353e4455e4dcea96a96ece4
22 (OVER S∞∞N) <75764401+22OVERSooN@users.noreply.github.com>
2022-10-31 15:02:32 +1100

    change the package name

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='distributions',
+setup(name='dis_reconnect',
       version='1.2',
       description='Gaussian and Binomial distributions',
-      packages=['dsnd_distributions'],
+      packages=['dsnd_probability'],
       author = 'Echo Yu',
       author_email = 'echoyu.business@gmail.com',
       zip_safe=False)
